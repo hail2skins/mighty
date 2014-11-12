7ceacccdee09a15bf13e19fe4cad1e0ae99e991a
Art Mills <art@hamcois.com>
2014-11-12 23:15:11 +0000

    Stuff is still broken, but closer.   Deal controller needs tweaking.

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,21 +1,34 @@
 class DealsController < ApplicationController
   before_action :get_customer_business_and_owner
-  
+  after_action :get_package, only: :create
   
   def index
   end
 
   def new
-    @deal = @customer.deals.new
+    @deal = @customer.deals.build
 
   end
 
   def create
+    @deal = @customer.deals.new(deal_params)
+ 
+    respond_to do |format|
+      if @deal.save
+        format.html { redirect_to [@owner, @business], notice: "Package purchased for " + @customer.name }
+      else
+        format.html { render action: 'new' }
+      end
+    end
   end
+    
+    
+    
+
   
   private
-      def visit_params
-        params.require(:deals).permit(:date_purchased, :date_completed, :used_count)
+      def deal_params
+        params.require(:deal).permit(:date_purchased, :date_completed, :used_count, :customer_id, :package_id)
       end
       
       def get_customer_business_and_owner
@@ -23,4 +36,10 @@ class DealsController < ApplicationController
         @business = @customer.business
         @owner = @business.owner
       end
+      
+      def get_package
+        @package = Package.first
+       	@deal.update_attribute(:used_count, @package.count)
+      end
+      
 end
