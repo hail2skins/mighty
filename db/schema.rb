e45fddbbc90087e1b09120ced278ea9dc9a8fd39
Art Mills <art@hamcois.com>
2014-12-08 23:49:39 +0000

    Working on adding service selection to package.   Migration added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121024939) do
+ActiveRecord::Schema.define(version: 20141208234737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20141121024939) do
     t.integer  "business_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "service_id"
   end
 
   add_index "packages", ["business_id"], name: "index_packages_on_business_id", using: :btree
