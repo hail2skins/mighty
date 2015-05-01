2cc56d2a7f6e9b0dcfd29da5479fe9fc8f5a6c67
Art Mills <art@hamcois.com>
2015-05-01 20:48:09 +0000

    Updated minitest feature for visiting and auto push to Heroku

--- test/integration/visitor_read_test.rb
+++ test/integration/visitor_read_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class VisitorReadTest < ActionDispatch::IntegrationTest
-  test "visit home page as a visotor" do
+  def setup
+    visit root_path
+  end
+  
+  test "visit home page as a visitor" do
     visit root_path
     
     assert page.has_title?("Mighty Smalls"), "Title -- Mighty Smalls not available."
@@ -15,8 +19,43 @@ class VisitorReadTest < ActionDispatch::IntegrationTest
                 "Sign up now!"
                 
     #page content
-    check_content "Small Businesses Gone Mighty!"
+    check_content "Small Businesses Gone Mighty!",
+                  "SITE IS CURRENTLY IN CLOSED ALPHA TESTING."
+  end
+  
+  test "checking the Help link" do 
+   click_link "Help"
+    
+   assert_equal(help_path, current_path)
+   assert_title("Help")
+   check_content "Help",
+                 "We will be providing a thorough help page 
+                 with instructions on how to use the site as 
+                 we near opening it to beta testing."
     
   end
+  
+  test "checking the About link" do
+    click_link "About"
+    
+    assert_equal(about_path, current_path)
+    assert_title "About"
+    check_content "About",
+                  "About Mighty Smalls"
+                  
+    within(".about") do
+     check_content "Small business, big features"
+    end
+  end
+  
+  test "checking the Contact link" do
+    click_link "Contact"
+    
+    assert_equal contact_path, current_path
+    assert_title "Contact"
+    check_content "Contact",
+                  "We will have a standard contact form provided on this page as we near beta testing."
+  end
+  
 
 end
