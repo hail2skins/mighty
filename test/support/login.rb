cf0f39f528d62e12aa7e508312a746f6a9d72028
Art Mills <art@hamcois.com>
2015-05-06 02:57:59 +0000

    Business adds services test created.

--- test/support/login.rb
+++ test/support/login.rb
@@ -5,3 +5,19 @@ def login
     fill_in 'Password', with: "password"
     click_button 'Login'
 end
+
+def new_login
+  visit login_path
+    
+    fill_in 'Email', with: owners(:login_owner).email
+    fill_in 'Password', with: "password"
+    click_button 'Login'
+end
+
+def service_test_login
+  visit login_path
+    
+    fill_in 'Email', with: owners(:service_test_owner).email
+    fill_in 'Password', with: "password"
+    click_button 'Login'
+end
--- test/support/new_login.rb
+++
@@ -1,7 +0,0 @@
-def new_login
-  visit login_path
-    
-    fill_in 'Email', with: owners(:login_owner).email
-    fill_in 'Password', with: "password"
-    click_button 'Login'
-end
