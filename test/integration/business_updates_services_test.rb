0e3a9fea986ae80e9e0ab0cf9a8e41911e89fcf8
Art Mills <art@hamcois.com>
2015-05-19 19:16:47 +0000

    Working up updating business services tests.

--- test/integration/business_updates_services_test.rb
+++ test/integration/business_updates_services_test.rb
@@ -9,4 +9,11 @@ class BusinessUpdatesServicesTest < ActionDispatch::IntegrationTest
     click_link "Logout"
   end
   
+  test "updating services from service index" do
+    click_link "Number of services:"
+    click_link "Microderm"
+    check_links "Edit this service"
+    
+  end
+  
 end
