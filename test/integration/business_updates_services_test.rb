201c15b8cbe0520a431e44b183b6548bce1b9b2c
Art Mills <art@hamcois.com>
2015-05-21 20:19:28 +0000

    Test for Jonathan

--- test/integration/business_updates_services_test.rb
+++ test/integration/business_updates_services_test.rb
@@ -12,6 +12,9 @@ class BusinessUpdatesServicesTest < ActionDispatch::IntegrationTest
   test "updating services from service index" do
     click_link "Number of services:"
     click_link "Microderm"
+    check_content "Microderm",
+                  "Making a pretty face",
+                  "$125.00"
     check_links "Edit this service"
     click_link "Edit this service"
     
@@ -19,6 +22,21 @@ class BusinessUpdatesServicesTest < ActionDispatch::IntegrationTest
     assert_field "Service name"
     assert_field "Description"
     assert_field "Price"
+    
+    fill_in "Service name", with: "Abrasion"
+    fill_in "Description", with: "The prettiest face there is."
+    fill_in "Price", with: "130"
+    click_button "Update Service"
+    
+    assert_equal owner_business_path(owner, business), current_path
+    click_link "Number of services:"
+    click_link "Abrasion"
+    check_content "Abrasion",
+                  "The prettiest face there is.",
+                  "$130.00"
+    save_and_open_page
+    
+    refute page.has_content?("Microderm"), "Microderm is still available."
     
   end
   
